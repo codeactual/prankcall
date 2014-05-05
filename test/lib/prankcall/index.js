c11fa968dd9a1f875f842dd41f2b45b0af8a60e6
David Smith <codeactual@gmail.com>
2014-05-05 21:36:47 +0000

    feat(lib): Init #start, #sleep

--- test/lib/prankcall/index.js
+++ test/lib/prankcall/index.js
@@ -1,35 +1,47 @@
 var T = require('../..');
 
-describe('Prankcall', function() {
+describe('Prankcall', function(testDone) {
   'use strict';
 
   beforeEach(function() {
+    this.producedData = [1, 2, 3];
   });
 
-  it.skip('should use default sleep', function() {
+  it('should provide consumer with result', function *() {
+    function *producer() {
+      yield noOp();
+      return this.producedData;
+    }
+
+    (yield T.prankcall.start(producer.bind(this))).should.deep.equal(this.producedData);
   });
 
-  it.skip('should use custom sleep', function() {
+  it.skip('should use default sleep', function *() {
   });
 
-  it.skip('should use default timeout', function() {
+  it.skip('should use custom sleep', function *() {
   });
 
-  it.skip('should use custom timeout', function() {
+  it.skip('should use default timeout', function *() {
   });
 
-  it.skip('should use no request params by default', function() {
+  it.skip('should use custom timeout', function *() {
   });
 
-  it.skip('should use custom request params', function() {
+  it.skip('should use no request params by default', function *() {
   });
 
-  it.skip('should use default backoff options', function() {
+  it.skip('should use custom request params', function *() {
   });
 
-  it.skip('should use custom backoff options', function() {
+  it.skip('should use default backoff options', function *() {
   });
 
-  it.skip('should resume queue after backoff ends', function() {
+  it.skip('should use custom backoff options', function *() {
+  });
+
+  it.skip('should resume queue after backoff ends', function *() {
   });
 });
+
+function noOp() { return function(done) { done(); }; }
