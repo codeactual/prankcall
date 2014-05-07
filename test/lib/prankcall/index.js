bdfcddd8226701abb7949392620956828de748a9
David Smith <codeactual@gmail.com>
2014-05-07 22:57:32 +0000

    refactor(lib): Remove unused co dep; clean up

--- test/lib/prankcall/index.js
+++ test/lib/prankcall/index.js
@@ -1,5 +1,4 @@
 var T = require('../..');
-var co = require('co');
 var sleep = T.prankcall.Prankcall.sleep;
 
 describe('Prankcall', function(testDone) {
@@ -8,30 +7,51 @@ describe('Prankcall', function(testDone) {
   beforeEach(function() {
     var test = this;
 
-    this.actualCallReturn = [];
+    // `this.send` will return one element at a time
+    this.expectedCallReturn = [1, 2, 3];
+
+    // To force retries
+    this.generatorErrMsg = 'test generator error';
+    this.generatorWithError = function *() {
+      if (true) { // Hide the `throw` from jshint
+        throw new Error(test.generatorErrMsg);
+      }
+      yield 'make jshint happy';
+    };
+
+    this.prankcall = T.prankcall.create();
+    this.prankcall.set('sleep', 1);
+
+    // Stand-in for something async like an HTTP request
     this.send = function *() {
       yield sleep(1);
       return test.expectedCallReturn[test.receiveCount];
     };
+
+    // Store values passed to `this.receive`
+    this.actualCallReturn = [];
+
+    // Stand-in for something async, ex. write `this.send` results to DB
     this.receive = function *(callReturn) {
       yield sleep(1);
       test.actualCallReturn.push(callReturn);
       return test.receiveCount++ < test.expectedCallReturn.length - 1;
     };
+
+    // To verify devs can omit the final `return` safely (no infinite loop)
     this.receiveWithoutReturn = function *(callReturn) {
       test.actualCallReturn.push(callReturn);
       yield sleep(1);
     };
+
+    // Cursor used to read `this.expectedCallReturn` one element at a time
     this.receiveCount = 0;
   });
 
-  it('should provide receive with all results', function(testDone) {
-    co(function *() {
-      this.prankcall.receive(this.receive);
-      yield this.prankcall.send(this.send);
-      this.actualCallReturn.should.deep.equal(this.expectedCallReturn);
-      testDone();
-    }).call(this);
+  it('should provide receive with all results', function *() {
+    this.prankcall.receive(this.receive);
+    yield this.prankcall.send(this.send);
+    this.actualCallReturn.should.deep.equal(this.expectedCallReturn);
   });
 
   it('should propagate send exception', function() {
