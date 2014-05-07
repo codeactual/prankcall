ff7a3a9c907e95dec5d6c2b923655d8fc1d51079
David Smith <codeactual@gmail.com>
2014-05-07 02:22:18 +0000

    feat(lib): Catch producer exceptions
    For later retry attempt

--- test/lib/prankcall/index.js
+++ test/lib/prankcall/index.js
@@ -4,43 +4,54 @@ describe('Prankcall', function(testDone) {
   'use strict';
 
   beforeEach(function() {
-    this.producedData = [1, 2, 3];
+    var test = this;
+
+    this.produced = [1, 2, 3];
+    this.producer = function *() {
+      for (var pos = 0; pos < test.produced.length; pos++) {
+        yield test.produced[pos];
+      }
+    };
   });
 
-  it('should provide consumer with result', function *() {
-    function *producer() {
-      yield noOp();
-      return this.producedData;
-    }
-
-    (yield T.prankcall.start(producer.bind(this))).should.deep.equal(this.producedData);
-  });
-
-  it.skip('should use default sleep', function *() {
-  });
-
-  it.skip('should use custom sleep', function *() {
+  it('should emit producer results via #next', function() {
+    var gen = T.prankcall.start(this.producer);
+    var prod = gen.next();
+    prod.value.should.equal(this.produced[0]);
+    prod = gen.next();
+    prod.value.should.equal(this.produced[1]);
+    prod = gen.next();
+    prod.value.should.equal(this.produced[2]);
+    prod = gen.next();
+    prod.done.should.be.ok;
   });
 
   it.skip('should use default timeout', function *() {
+    yield true;
   });
 
   it.skip('should use custom timeout', function *() {
+    yield true;
   });
 
   it.skip('should use no request params by default', function *() {
+    yield true;
   });
 
   it.skip('should use custom request params', function *() {
+    yield true;
   });
 
   it.skip('should use default backoff options', function *() {
+    yield true;
   });
 
   it.skip('should use custom backoff options', function *() {
+    yield true;
   });
 
   it.skip('should resume queue after backoff ends', function *() {
+    yield true;
   });
 });
 
